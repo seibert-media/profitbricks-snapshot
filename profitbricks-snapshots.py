adc4a7e67d487a278318a45a16b66ccd40a0df33
Benjamin Kendinibilir <bkendinibilir@seibert-media.net>
2016-03-20 18:34:09 +0100

    initial commit

---
+++ profitbricks-snapshots.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import configparser
+from datetime import datetime, timedelta
+from profitbricks.client import ProfitBricksService
+
+# read config
+config = configparser.ConfigParser()
+config.read('settings.cfg')
+
+datacenter_id = config.get('snapshots', 'datacenter_id')
+snapshot_prefix = config.get('snapshots', 'snapshot_prefix', fallback="AUTOSNAP")
+retention_time = config.getint('snapshots', 'retention_time', fallback=7)
+
+pb = ProfitBricksService(
+    username=config.get('credentials', 'username'),
+    password=config.get('credentials', 'password'),
+)
+
+# timestamp for creating and deleting
+now = datetime.now()
+
+# create new snapshots from list of volumes in datacenter
+volumes = pb.list_volumes(datacenter_id=datacenter_id)
+
+for volume_item in volumes['items']:
+    volume_id = volume_item['id']
+    volume = pb.get_volume(datacenter_id, volume_id)
+
+    volume_name = volume['properties']['name']
+    snapshot_name = "{}-{}-{}".format(snapshot_prefix, now.strftime("%Y%m%d"), volume_name)
+
+    print("creating snapshot: {}".format(snapshot_name))
+    pb.create_snapshot(datacenter_id, volume_id, snapshot_name)
+
+# delete old snapshots
+snapshots = pb.list_snapshots()
+
+for snapshot_item in snapshots['items']:
+    snapshot_id = snapshot_item['id']
+    snapshot = pb.get_snapshot(snapshot_id)
+
+    snapshot_name = snapshot['properties']['name']
+    if not snapshot_name.startswith(snapshot_prefix):
+        continue
+
+    snapshot_created = snapshot['metadata']['createdDate']
+    snapshot_date = datetime.strptime(snapshot_created, "%Y-%m-%dT%H:%M:%SZ")
+
+    if snapshot_date < now - timedelta(days=retention_time):
+        print("deleting snapshot: {} (created: {})".format(snapshot_name, snapshot_created))
+        pb.delete_snapshot(snapshot_id)
